ed2ca31ef0cd9f2f360294f037c26d67daafcc0b
renzoSalosagcol <renzosalosagcol@csu.fullerton.edu>
2024-03-13 15:10:12 -0700

    Check each char in the line

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# Pseudocode:
-# Function: lexer
-#
-# Input: input_scode.txt (in C/C++/Java/Python)
-# Output: Document named "output" That is separated into two columns
-#         One column for tokens and another for lexemes
-#
-# function named "lexer" that takes the input of txtDoc
-#     for eachWord in txtDoc
-#         print eachWord on "Lexeme" column
-#         print eachWord's appropriate token
 from curses.ascii import isalpha, isdigit
-
 
 def lexer(input_scode):
 
